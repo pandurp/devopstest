930e5d6b9f7e8137c1264a595787edb2b3b082aa
pandurp <pandu.pabbisetty@hcl.com>
2015-10-07 17:33:47 -0700

    Added a new print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld
 		System.out.println("Hello World!");
 		System.out.println("Hello DevOpsTest Team, you have a great program going\n");
 		System.out.println("Hello - this is to show changes made to Java code get into SCM, to Build to Target server via Chef\n");
+		System.out.println("Hello DevOpsTest Team, you have a great program going...Rajeev Demo\n");
 	}
 }
