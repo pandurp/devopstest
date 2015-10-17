2b757f26b1b71007d3e815ff3f175980c5dae447
pandurp <pandu.pabbisetty@hcl.com>
2015-10-17 11:31:27 -0700

    Added couple of lines to better format output

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,9 +4,11 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
+		System.out.println("\n\n=======================================================================\n");
 		System.out.println("Hello World! Good Morning Asia, Good Evening Americas...what a great time!");
 		System.out.println("Hello DevOpsTest Team, you have a great program going\n");
 		System.out.println("Hello - this is to show changes made to Java code get into SCM, to Build to Target server via Chef\n");
 		System.out.println("Hello DevOpsTest Team, you have a great program going...Rajeev Demo\n");
+		System.out.println("\n\n=======================================================================\n");
 	}
 }
