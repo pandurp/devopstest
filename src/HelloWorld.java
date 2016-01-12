23061145a20ef58b825624d9b60e31f86a8185a1
pandurp <pandu.pabbisetty@hcl.com>
2016-01-12 14:51:28 -0800

    Added a new print stmt 12-Jan-16
    See above...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld
 	public static void main(String[] args) {
 		System.out.println("\n\n=======================================================================\n");
 		System.out.println("Happy New Year 2016!\n");
+		System.out.println("Preparing for a demo on 12-Jan-2016...added this to show changes!\n");
 		System.out.println("Hello World! Good Morning Asia, Good Evening Americas...what a great time!.....\n");
 		System.out.println("demo...in....progress....Hello DevOpsTest Team, you have a great program going\n");
 		System.out.println("Hello - this is to show changes made to Java code get into SCM, to Build to Target server via Chef\n");
