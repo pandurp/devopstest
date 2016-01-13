5120141ca41315c47bf4fda9d10b14d9d818e2eb
pandurp <pandu.pabbisetty@hcl.com>
2016-01-13 07:04:49 -0800

    Added new greeting for 13-Jan-16
    See above.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,12 +5,13 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("\n\n=======================================================================\n");
+		System.out.println("Happy Pongal, Kanuma! 13-Jan-2016!\n");
 		System.out.println("Happy New Year 2016!\n");
 		System.out.println("Preparing for a demo on 12-Jan-2016...added this to show changes!\n");
 		System.out.println("Hello World! Good Morning Asia, Good Evening Americas...what a great time!.....\n");
 		System.out.println("demo...in....progress....Hello DevOpsTest Team, you have a great program going\n");
 		System.out.println("Hello - this is to show changes made to Java code get into SCM, to Build to Target server via Chef\n");
-		System.out.println("Hello DevOpsTest Team, you have a great program going...Rajeev Demo\n");
+		System.out.println("Hello DevOpsTest Team, you have a great program going... Demo\n");
 		System.out.println("Hello DevOpsTest Team, Dry run with Rajeev 07-Oct-15....05.36PM\n");
 		System.out.println("\n\n=======================================================================\n");
 	}
